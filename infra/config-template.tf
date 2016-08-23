8e82a3305a5739283bc14d5642b36e60bafc907c
Pelayo Sanchez Margareto <sanchezmargareto@gmail.com>
2016-08-23 13:54:56 +0200

    Including empty namespace on slack setting.

--- infra/config-template.tf
+++ infra/config-template.tf
@@ -4,6 +4,7 @@ resource "aws_elastic_beanstalk_configuration_template" "hubot-config" {
   solution_stack_name = "64bit Amazon Linux 2016.03 v2.1.3 running Docker 1.11.1"
 
   setting {
+    namespace = ""
     name = "HUBOT_SLACK_TOKEN"
     value = "${var.hubot_slack_token}"
   }
