bb15d67163b0a0e0a331d1fafe97a0fe45b6bef2
Pelayo Sanchez Margareto <sanchezmargareto@gmail.com>
2016-08-23 12:14:33 +0200

    Including remote state and access vars.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,15 +1,28 @@
 #!/usr/bin/env bash
 
 project_dir=$(pwd)
-
-access_key=${AWS_ACCESS_KEY_ID:-none}
-secret_key=${AWS_SECRET_ACCESS_KEY:-none}
-
 environment=${ENVIRONMENT:-r2d2}
+
+access_key_deployment=${AWS_ACCESS_KEY_ID:-none}
+secret_key_deployment=${AWS_SECRET_ACCESS_KEY:-none}
+region_deployment=${AWS_REGION:-none}
+
+access_key_states=${AWS_ACCESS_KEY_ID_S3:-none}
+secret_key_states=${AWS_SECRET_ACCESS_KEY_S3:-none}
+region_states=${AWS_REGION_S3:-none}
+bucket_states=${STATES_BUCKET:-none}
+state_key="/sam/sam-hubot/${environment}.tfstate"
+
 
 if [ ! -f vendor/terraform/terraform ]; then
     bash scripts/terraform.sh
 fi
 
+./vendor/terraform/terraform remote config \
+    -backend=s3 \
+    -backend-config="bucket=${bucket_states}" \
+    -backend-config="key=${state_key}" \
+    -backend-config="region=${region_states}"
+
 ./vendor/terraform/terraform get -update=true terraform
-./vendor/terraform/terraform apply -var environment=$environment -var access_key=$access_key -var secret_key=$secret_key -state $environment.tfstate terraform
+./vendor/terraform/terraform apply -var environment=$environment -var access_key=$access_key_deployment -var secret_key=$secret_key_deployment infra
