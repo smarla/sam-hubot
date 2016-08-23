d43b97d250352591a3d58ac97611c917d8600c02
Pelayo Sanchez Margareto <sanchezmargareto@gmail.com>
2016-08-23 17:33:39 +0200

    Sourcing vars.

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -6,6 +6,9 @@ project_dir=$(pwd)
 gpg --import /var/snap-ci/repo/deployments-gpg
 git crypt unlock
 
+# Sourcing vars
+source $project_dir/scripts/.deployment-profile
+
 # Terraform
 if [ ! -f vendor/terraform/terraform ]; then
     bash $project_dir/scripts/dependencies/terraform.sh
