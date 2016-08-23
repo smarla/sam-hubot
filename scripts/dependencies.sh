d3ed330f269813c5f37be69a7549e6c3e4534f72
Pelayo Sanchez Margareto <sanchezmargareto@gmail.com>
2016-08-23 17:24:25 +0200

    Unlocking repo upon build.

--- scripts/dependencies.sh
+++ scripts/dependencies.sh
@@ -2,6 +2,10 @@
 
 project_dir=$(pwd)
 
+# GPG Keys & Repo unlocking
+gpg --import /var/snap-ci/repo/deployments-gpg
+git crypt unlock
+
 # Terraform
 if [ ! -f vendor/terraform/terraform ]; then
     bash $project_dir/scripts/dependencies/terraform.sh
